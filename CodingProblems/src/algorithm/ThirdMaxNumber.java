49057b84654d56205b42fd66cbe46a5515b7d5d6
ksumi <ksumi@DESKTOP-C81D9FO>
2020-02-12 23:28:39 +0900

    Two other ways: using TreeSet and Integer wrapper class

--- CodingProblems/src/algorithm/ThirdMaxNumber.java
+++ CodingProblems/src/algorithm/ThirdMaxNumber.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class ThirdMaxNumber {
 
@@ -39,4 +40,52 @@ public class ThirdMaxNumber {
 		}
 		return max;
 	}
+
+	public static int getThirdMax(int[] numbers) {
+		// TreeSet: store values in ascending order
+		TreeSet<Integer> set = new TreeSet<>();
+		
+		for (int n : numbers) {
+			set.add(n);
+		}
+		
+		// if the given array has less than three numbers 
+		// return the max number
+		if (set.size() < 3)
+			return set.last();
+		
+		// remove first and second max numbers
+		set.remove(set.last());
+		set.remove(set.last());
+		
+		return set.last();
+	}
+
+	public int getThirdMaxValue(int[] num) {
+		Integer max1 = null;
+		Integer max2 = null;
+		Integer max3 = null;
+		
+		for (Integer n : num) {
+			
+			// when there are duplicate values in array continue the loop
+			if (n.equals(max1) || n.equals(max2) || n.equals(max3))
+				continue;
+			
+			if (max1 == null || n > max1) {
+				max3 = max2;
+				max2 = max1;
+				max1 = n;
+			} else if (max2 == null || n > max2) {
+				max2 = max1;
+				max2 = n;
+			} else if (max3 == null || n > max3) {
+				max3 = n;
+			}
+		}
+		// max3 being null means there are less than 3 values in the given array
+		// in that case, return the max value (not third max)
+		return max3 == null ? max1 : max3;
+	}
+	
 }
