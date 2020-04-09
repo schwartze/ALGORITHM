77299134f4e3600eed8cd2a7562e1eb1db2afebe
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-04-09 16:43:20 +0900

    find the max sum of minimum values of n-pairs of integers

---
+++ CodingProblems/src/algorithm/ArrayPartition.java
@@ -0,0 +1,36 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class ArrayPartition {
+
+	public static void main(String[] args) {
+		ArrayPartition partition = new ArrayPartition();
+		int[] nums = {1, 4, 3, 2};
+		int result = partition.arrayPairSum(nums);
+		System.out.println(result);
+	}
+	
+	/*
+	 * Given an array of 2n integers, your task is to group these integers 
+	 * into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) 
+	 * which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+	 */
+	
+    public int arrayPairSum(int[] nums) {
+        
+    	Arrays.sort(nums);
+    	
+    	int result = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		
+    		if (i % 2 != 0)
+    			continue;
+    		
+    		result += nums[i];
+    	}
+    	
+    	return result;
+    }
+}
