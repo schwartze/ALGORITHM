205046dc4a18c0cffe961da81cbefabd38c8bb81
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-04-20 22:55:18 +0900

    Brute Force method has been added

--- CodingProblems/src/algorithm/ShortestUnsortedContinuousSubarray.java
+++ CodingProblems/src/algorithm/ShortestUnsortedContinuousSubarray.java
@@ -8,7 +8,7 @@ public class ShortestUnsortedContinuousSubarray {
 		ShortestUnsortedContinuousSubarray sucs = new ShortestUnsortedContinuousSubarray();
 		int[] nums = {2, 6, 4, 8, 10, 9, 15};
 		
-		int result = sucs.findUnsortedSubarray2(nums);
+		int result = sucs.findUnsortedSubarray3(nums);
 		System.out.println(result);
 	}
 	/*
@@ -50,26 +50,40 @@ public class ShortestUnsortedContinuousSubarray {
     	int[] temp = nums.clone();
     	
     	Arrays.sort(temp);
-    	int begIdx = 0;
-    	int endIdx = 0;
-    	// {2, 6, 4, 8, 10, 9, 15}
-    	// {2, 4, 6, 8, 9, 10, 15}
+    	
+    	int begIdx = nums.length, endIdx = 0;
+    	
+    	// nums = {2, 6, 4, 8, 10, 9, 15}
+    	// temp = {2, 4, 6, 8, 9, 10, 15}
+    	
     	for (int i = 0; i < nums.length; i++) {
     		
     		if (nums[i] != temp[i]) {
-    			begIdx = i;
-    			break;
+    			begIdx = Math.min(begIdx, i);
+    			endIdx = Math.max(endIdx, i);
     		}
     	}
     	
-    	for (int j = nums.length - 1; j >= 0; j--) {
+    	return (endIdx - begIdx >= 0) ? endIdx - begIdx + 1 : 0;
+    }
+    
+    // Brute Force
+    public int findUnsortedSubarray3(int[] nums) {
+    	
+    	int start = nums.length; 
+    	int end = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
     		
-    		if (nums[j] != temp[j]) {
-    			endIdx = j;
-    			break;
+    		for (int j = i + 1; j < nums.length - 1; j++) {
+    			
+    			if (nums[i] > nums[j]) {
+    				start = Math.min(start, i);
+    				end = Math.max(end, j);
+    			}
     		}
     	}
     	
-    	return (endIdx == 0 && begIdx == 0) ? 0 : endIdx - begIdx + 1;
+    	return end - start < 0 ? 0 : end - start + 1;
     }
 }
