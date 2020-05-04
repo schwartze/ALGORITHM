2d9be1c7a32953155cbf0aec306e487e21fcb0b8
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-05-04 17:19:10 +0900

    with two lists

--- CodingProblems/src/algorithm/AvgBinaryTree.java
+++ CodingProblems/src/algorithm/AvgBinaryTree.java
@@ -8,14 +8,20 @@ public class AvgBinaryTree {
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(3);
-		root.left = new TreeNode(9);
-		root.right = new TreeNode(20);
+		root.left = new TreeNode(1);
+		root.right = new TreeNode(5);
 		
-		root.right.left = new TreeNode(15);
-		root.right.right = new TreeNode(7);
+		root.left.left= new TreeNode(0);
+		root.left.right = new TreeNode(2);
+		
+		root.right.left = new TreeNode(4);
+		root.right.right = new TreeNode(6);
+		
+		TreeNode root2 = new TreeNode(1);
+		root2.left = new TreeNode(1);
 		
 		AvgBinaryTree avgBinaryTree = new AvgBinaryTree();
-		List<Double> res = avgBinaryTree.averageOfLevels(root);
+		List<Double> res = avgBinaryTree.avgOfTreeLevels(root);
 		
 		for (double d : res) {
 			System.out.print(d + ", ");
@@ -48,9 +54,45 @@ public class AvgBinaryTree {
     	int left = helper(node.left, avg);
     	int right = helper(node.right, avg);
     	
-    	if (left != 0 && right != 0) 
-    		avg.add((double)(left + right) / 2);
+    	int num = 0;
+    	
+    	num = left == 0 ? num : num + 1;
+    	num = right == 0 ? num : num + 1;
+    	    	
+    	if (left != 0 || right != 0) 
+    		avg.add((double)(left + right) / num);
     	
     	return node.val;
+    }
+    
+    public List<Double> avgOfTreeLevels(TreeNode root) {
+    	
+    	List<Integer> count = new ArrayList<>();
+    	List<Double> res = new ArrayList<>();
+    	
+    	avg(root, 0, res, count);
+    	
+    	for (int i = 0; i < res.size(); i++) {
+    		res.set(i, res.get(i) / count.get(i));
+    	}
+    	
+    	return res;
+    }
+    
+    private void avg(TreeNode node, int i, List<Double> sum, List<Integer> count) {
+    	
+    	if (node == null)
+    		return;
+    	
+    	if (i < sum.size()) {
+    		sum.set(i, sum.get(i) + node.val);
+    		count.set(i, count.get(i) + 1);
+    	} else {
+    		sum.add(1.0 * node.val);
+    		count.add(1);
+    	}
+    	
+    	avg(node.left, i + 1, sum, count);
+    	avg(node.right, i + 1, sum, count);
     }
 }
