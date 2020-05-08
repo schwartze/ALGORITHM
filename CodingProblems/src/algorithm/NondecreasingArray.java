365d878cd018b3fdcfe5b2f7fafd1b375c348362
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-05-08 22:31:14 +0900

    solution

--- CodingProblems/src/algorithm/NondecreasingArray.java
+++ CodingProblems/src/algorithm/NondecreasingArray.java
@@ -3,7 +3,7 @@ package algorithm;
 public class NondecreasingArray {
 
 	public static void main(String[] args) {
-		int[] nums = {4, 2, 3};
+		int[] nums = {3, 4, 2, 3};
 		
 		NondecreasingArray nondecreasing = new NondecreasingArray();
 		boolean result = nondecreasing.checkPossibility(nums);
@@ -24,11 +24,23 @@ public class NondecreasingArray {
     				result = nums[i] <= nums[i + 2];
     			else 
     				result = nums[i] <= nums[i + 1];
-
-    			if (result)
-    				return result;
     		}
+			if (result)
+				return result;
     	}
     	return false;
+    }
+    
+    public boolean checkPossibility2(int[] nums) {
+        int p = -1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] > nums[i]) {
+                if (p != -1) return false;
+                else p = i;
+            }
+        }
+        return (p == -1 ||   // no reversed pair
+                p == 1 || p == nums.length - 1 ||  // reversed pair is first or last element
+                nums[p - 2] <= nums[p] || nums[p - 1] <= nums[p + 1]); // normal case range [p-2 --> p+1] all valid
     }
 }
