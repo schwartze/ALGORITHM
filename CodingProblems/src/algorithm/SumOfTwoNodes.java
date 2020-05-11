414a39aa220d2f1ea2d48ae71d18e4e7a3f6e7a0
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-05-11 20:53:03 +0900

    two different solutions added

--- CodingProblems/src/algorithm/SumOfTwoNodes.java
+++ CodingProblems/src/algorithm/SumOfTwoNodes.java
@@ -1,11 +1,12 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class SumOfTwoNodes {
 
-	
+	// #1. Ssing helper class with a list 
 	public boolean findTarget(TreeNode root, int target) {
 		
 		if (root == null)
@@ -35,5 +36,57 @@ public class SumOfTwoNodes {
 		list.add(node.val);
 		helper(node.left, list);
 		helper(node.right, list);
+	}
+	
+	// #2. With a list but using while instead of nested for statement
+	public boolean findTarget2(TreeNode node, int target) {
+		
+		if (node == null)
+			return false;
+		
+		List<Integer> list = new ArrayList<>();
+		
+		helper(node, list);
+		
+		int l = 0, r = list.size() - 1;
+		
+		while (r > l) {
+			
+			int sum = list.get(r) + list.get(l);
+			
+			if (sum == target)
+				return false;
+			
+			if (sum > target)
+				r--;
+			else 
+				l++;
+		}
+		
+		return false;
+	}
+	
+	// #3. Using set 
+	public boolean findTarget3(TreeNode root, int k) {
+		
+		if (root == null)
+			return false;
+		
+		HashSet<Integer> set = new HashSet<>();
+		
+		return find(root, k, set);
+	}
+	
+	private boolean find(TreeNode node, int k, HashSet<Integer> set) {
+		
+		if (node == null)
+			return false;
+		
+		if (set.contains(k - node.val))
+			return true;
+		
+		set.add(node.val);
+		
+		return find(node.left, k, set) || find(node.right, k, set);
 	}
 }
