22904ab024766178e83ae8dea6141f24bbb32ba9
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-05-19 22:28:45 +0900

    using StringBuilder

--- CodingProblems/src/algorithm/RepeatedString.java
+++ CodingProblems/src/algorithm/RepeatedString.java
@@ -4,23 +4,26 @@ public class RepeatedString {
 	
 	public static void main(String[] args) {
 		RepeatedString repeatedStr = new RepeatedString();
-		int n = repeatedStr.repeatedTimes("abcd", "cdabcdab");
+		int n = repeatedStr.repeatedTimes("abc", "cabcabca");
 		System.out.println(n);
 	}
 	// find out how many times A should be repeated for B to be a substring of repeated A
 	public int repeatedTimes(String A, String B) {
 		
-		String[] arr = B.replaceAll(A, "-").split("-");
-		int count = 0;
+		int count = 1;
 		
-		for (String str : arr) {
-			
-			if (A.indexOf(str) == -1)
-				return -1;
-			count++;
+		StringBuilder sb = new StringBuilder(A);
+		
+		for (; sb.length() < B.length(); count++) {
+			sb.append(A);
 		}
 		
-		return count + arr.length - 1;
+		if (sb.indexOf(B) >= 0)
+			return count;
+		
+		if (sb.append(A).indexOf(B) >= 0)
+			return count + 1;
+		
+		return -1;
 	}
-
 }
