bea97662f83dea66d72aac115ac255511a1df0c5
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-05-20 22:28:14 +0900

    another implementation using map

--- CodingProblems/src/algorithm/EmployeeImportance.java
+++ CodingProblems/src/algorithm/EmployeeImportance.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class EmployeeImportance {
@@ -25,7 +26,7 @@ public class EmployeeImportance {
 		emps.add(emp4);
 		
 		EmployeeImportance imp = new EmployeeImportance();
-		int n = imp.getImportance(emps, 1);
+		int n = imp.getImportance2(emps, 1);
 		System.out.println(n);
 	}
 	// calculate the total importance value of this employee and all his subordinates.
@@ -49,6 +50,31 @@ public class EmployeeImportance {
     					helper(employees, id, res);
     		}
     	}
+    }
+    
+    HashMap<Integer, Employee> emap;
+    public int getImportance2(List<Employee> employees, int id) {
+    	
+    	emap = new HashMap<>();
+    	
+    	for (Employee emp : employees) {
+    		emap.put(emp.id, emp);
+    	}
+    	
+    	return helper2(id);
+    }
+    
+    private int helper2(int id) {
+    	
+    	Employee emp = emap.get(id);
+    	int ans = emp.importance;
+    	
+    	if (emp.subordinates != null) {
+        	for (int subId : emp.subordinates) 
+        		ans += helper2(subId);
+    	}
+    	
+    	return ans;
     }
 }
 
