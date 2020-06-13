c483fb0dda1268e824f1d5bc6bb2cb5a798be87b
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-06-13 10:40:09 +0900

    eliminated an unnecessary variable

--- CodingProblems/src/algorithm/ToeplitzMatrix.java
+++ CodingProblems/src/algorithm/ToeplitzMatrix.java
@@ -25,10 +25,9 @@ public class ToeplitzMatrix {
 		
 		for (int i = 0; i < matrix.length - 1; i++) {
 			
-			int temp = i;
 			for (int j = 0; j < matrix[i].length - 1; j++) {
 				
-				if (matrix[i][j] != matrix[temp + 1][j+ 1])
+				if (matrix[i][j] != matrix[i + 1][j + 1])
 					return false;
 			}
 		}
