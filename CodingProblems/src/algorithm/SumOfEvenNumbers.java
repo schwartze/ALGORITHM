b19563476c1860eda5827b1dbd4b8ed953ccfcfb
KsumiKim <48200662+KsumiKim@users.noreply.github.com>
2020-08-20 21:58:35 +0900

    selectively add even element to sum

--- CodingProblems/src/algorithm/SumOfEvenNumbers.java
+++ CodingProblems/src/algorithm/SumOfEvenNumbers.java
@@ -11,7 +11,7 @@ public class SumOfEvenNumbers {
 				{2,3}
 				};
 		SumOfEvenNumbers evenNumbers = new SumOfEvenNumbers();
-		int[] res = evenNumbers.sumEvenAfterQueries(A, queries);
+		int[] res = evenNumbers.sumEvenAfterQueries2(A, queries);
 		
 		for (int n : res)
 			System.out.print(n + ", ");
@@ -30,6 +30,29 @@ public class SumOfEvenNumbers {
         			continue;
         		evenSum += A[j];
     		}
+    		res[i] = evenSum;
+    	}
+    	return res;
+    }
+    
+    public int[] sumEvenAfterQueries2(int[] A, int[][] queries) {
+    	int[] res = new int[A.length];
+    	int evenSum = 0;
+		
+		for (int n : A)
+			if (n % 2 == 0)
+				evenSum += n;
+		// since iterating all elements can be time consuming 
+    	for (int i = 0; i < A.length; i++) {
+    		int val = queries[i][0];
+    		int index = queries[i][1];
+
+    		if (A[index] % 2 == 0)
+    			evenSum -= A[index];
+    		A[index] += val;
+    		if (A[index] % 2 == 0)
+    			evenSum += A[index];
+
     		res[i] = evenSum;
     	}
     	return res;
