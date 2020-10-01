fb99db55eb62b4ca0913e45e85c3df425837c99c
KsumiKim <ksumiKim@gmail.com>
2020-10-01 16:41:32 +0900

    Return if the occurrences of each num in the given arr is unique

---
+++ CodingProblems/src/algorithm/UniqueOccurrences.java
@@ -0,0 +1,32 @@
+package algorithm;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueOccurrences {
+
+	public static void main(String[] args) {
+		// int[] arr = {1, 2, 2, 1, 1, 3};
+		// int[] arr = {1, 2};
+		int[] arr = {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0};
+		
+		UniqueOccurrences uOccurrences = new UniqueOccurrences();
+		boolean res = uOccurrences.uniqueOccurrences(arr);
+		System.out.println(res);
+	}
+	
+    public boolean uniqueOccurrences(int[] arr) {
+    	Map<Integer, Integer> map = new HashMap<>(arr.length);
+    	Set<Integer> set = new HashSet<>(map.size());
+
+    	for (int n : arr)
+    		map.put(n, map.getOrDefault(n, 0) + 1);
+    	
+    	for (int value : map.values())
+    		set.add(value);
+    	
+    	return map.size() == set.size();
+    }
+}
