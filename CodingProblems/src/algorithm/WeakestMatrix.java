a1dbf4cc83c2418ca9fbb7ab38548c33285bb259
KsumiKim <ksumiKim@gmail.com>
2020-10-23 23:19:16 +0900

    heap and binary search approach added

--- CodingProblems/src/algorithm/WeakestMatrix.java
+++ CodingProblems/src/algorithm/WeakestMatrix.java
@@ -1,8 +1,10 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 public class WeakestMatrix {
@@ -37,7 +39,7 @@ public class WeakestMatrix {
 		;
 		int k2 = 1;
 		int k = 3;
-		int[] res = kWeakestRows(mat2, k2);
+		int[] res = kWeakestRows2(mat, k);
 		
 		for (int n : res)
 			System.out.print(n + ", ");
@@ -82,5 +84,49 @@ public class WeakestMatrix {
 				break;
     	}
     	return res;
+    }
+    
+    
+    public static int[] kWeakestRows2(int[][] mat, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
+        int[] ans = new int[k];
+        
+        for (int i = 0; i < mat.length; i++) {
+            pq.offer(new int[] {numOnes(mat[i]), i});
+            if (pq.size() > k)
+                pq.poll();
+        }
+        
+        while (k > 0)
+            ans[--k] = pq.poll()[1];
+        
+        return ans;
+    }
+    
+    private static int numOnes(int[] row) {
+        int lo = 0;
+        int hi = row.length;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (row[mid] == 1)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo;
+    }
+    
+    class MyComparator implements Comparator<int[]>{
+        public int compare(int[] a, int[] b) {
+            int result = 0;
+            if(a[0] != b[0]){
+                result =  b[0] - a[0];
+            }else{
+                result = b[1] - a[1];
+            }
+            return result;
+        }
     }
 }
