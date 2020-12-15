2dcb17ad4fd057a91312a80e84ea55ff769fcd4c
KsumiKim <ksumiKim@gmail.com>
2020-12-15 20:24:33 +0900

    Adjustment has been made

--- CodingProblems/src/algorithm/CircularTrack.java
+++ CodingProblems/src/algorithm/CircularTrack.java
@@ -8,36 +8,58 @@ public class CircularTrack {
 	public static void main(String[] args) {
 		int[] rounds = {3,2,1,2,1,3,2,1,2,1,3,2,3,1};
 		int n = 3;
-		List<Integer> res = mostVisited(n, rounds);
+		
+		int[] rounds2 = {1, 3, 1, 2};
+		int n2 = 4;
+		
+		int[] rounds3 = {2,1,2,1,2,1,2,1,2};
+		int n3 = 2;
+		
+		int[] rounds4 = {1, 3, 5, 7};
+		int n4 = 7;
+		
+		int[] rounds5 = {2, 3, 2, 3};
+		int n5 = 4;
+		
+		int[] rounds6 = {3, 2};
+		int n6 = 5;
+		
+		
+		List<Integer> res = mostVisited(n6, rounds6);
 		
 		for (int num : res)
 			System.out.print(num + ", ");
 	}
 	
     public static List<Integer> mostVisited(int n, int[] rounds) {
-    	int[] temp = new int[n];
+    	int[] countArray = new int[n];
     	
+    	countArray[rounds[0] - 1]++;
     	for (int i = 0; i < rounds.length - 1; i++) {
-    		int k = i == rounds.length - 2 ? 1 : 0;
-    		for (int j = 0; j < (int)Math.abs(rounds[i] - rounds[i + 1]) + k; j++) {
-    			if ((rounds[i] + j) % n == 0) {
-    				temp[temp.length - 1]++;
-    				continue;
+    		int currentTrack = rounds[i];
+    		int nextTrack = rounds[i + 1];
+    		
+    		while (currentTrack != nextTrack) {
+    			if (currentTrack >= n) {
+    				currentTrack = 1;
+    			} else {
+    				currentTrack++;
     			}
-    			temp[(rounds[i] + j) % n - 1]++;
+    			countArray[currentTrack - 1]++;
     		}
     	}
-    	int max = 0;
+    		
+    	int maxCount = 0;
     	List<Integer> result = new ArrayList<>();
     	
-    	for (int round : temp)
-    		max = Math.max(round, max);
+    	for (int num : countArray)
+    		if (num > maxCount)
+    			maxCount = num;
     	
-    	for (int i = 0; i < temp.length; i++) {
-    		if (temp[i] == max) {
-				result.add(i + 1);
-    		}
-    	}
+    	for (int i = 0; i < countArray.length; i++)
+    		if (countArray[i] == maxCount)
+    			result.add(i + 1);
+    	
     	return result;
     }
 }
