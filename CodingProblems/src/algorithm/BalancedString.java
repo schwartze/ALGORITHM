34ec26802e9cf975ba788433fd7e13306e76f5ac
KsumiKim <ksumiKim@gmail.com>
2021-01-08 21:12:10 +0900

    Using L and R count variables

--- CodingProblems/src/algorithm/BalancedString.java
+++ CodingProblems/src/algorithm/BalancedString.java
@@ -3,38 +3,27 @@ package algorithm;
 public class BalancedString {
 
 	public static void main(String[] args) {
-		String s = "RLRRRLLRLL";
+		String s = "RLLLLRRRLR";
 		BalancedString bString = new BalancedString();
 		int res = bString.balancedStringSplit(s);
 		System.out.println(res);
 	}
-	
+    
     public int balancedStringSplit(String s) {
-    	int res = 0;
+    	int max = 0, rCnt = 0, lCnt = 0;
     	
     	for (int i = 0; i < s.length(); i++) {
-    		char c = s.charAt(i);
-        	int firstCnt = 1, secondCnt = 0;
-    		
-    		for (int j = i + 1; j < s.length(); j++) {
-    			
-    			if (c == s.charAt(j))
-    				firstCnt++;
-    			else 
-    				secondCnt++;
+    		if (s.charAt(i) == 'R')
+    			rCnt++;
+    		else 
+    			lCnt++;
 
-    			if (firstCnt < secondCnt) {
-    				i = j;
-    				break;
-    			}
-    			
-    			if (firstCnt == secondCnt) {
-    				i = j;
-    				break;
-    			}
+    		if (rCnt == lCnt) {
+    			max++;
+    			rCnt = 0;
+    			lCnt = 0;
     		}
-    		res++;
     	}
-    	return res;
+    	return max;
     }
 }
