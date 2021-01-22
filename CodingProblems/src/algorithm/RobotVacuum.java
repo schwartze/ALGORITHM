c6e4a1684b027b0910b64fdabc4c79ee72f17a86
KsumiKim <ksumiKim@gmail.com>
2021-01-23 01:53:57 +0900

    modified handling all cleaned area

--- CodingProblems/src/algorithm/RobotVacuum.java
+++ CodingProblems/src/algorithm/RobotVacuum.java
@@ -24,43 +24,45 @@ public class RobotVacuum {
 	}
 	
 	public int getCleanedArea(int r, int c, int d, int[][] area) {
+		// NESW (0123)
+		// d: N(0) -> c--
+		//    E(1) -> r++
+		//    S(2) -> c++
+		//    W(3) -> r--
 		int cleanedArea = 1;
-		area[r][c]++;
+		area[r][c]--;
 		int[] yx = {r, c};
 		
 		while (true) {
+			// NESW 모두 청소된 경우
 			if (allCleaned(yx, area)) {
-				int[] shifted = shiftBack(yx, cleanedArea, area);
-				if (yx[0] == shifted[0] && yx[1] == shifted[1]) 
+				int[] shifted = shiftBack(yx, d, area);
+				if (shifted[0] == -2 && shifted[1] == -2)
 					break;
-				
+
 				yx = shifted;
+				continue;
 			}
 			int[] left = cleanLeft(yx, d, area);
+			
 			// 왼쪽 청소 불가능: 왼쪽으로 방향 전환 후 재시도
 			if (left[0] == yx[0] && left[1] == yx[1]) {
 				d = d == 0 ? 3 : d - 1;
 				continue;
 			}
-			// 왼쪽 청소 가능
-			// 현 위치에서 왼쪽으로 방향 전환 
+			// 왼쪽 청소 가능: 현 위치에서 왼쪽으로 방향 전환 
 			d = d == 0 ? 3 : d - 1;
 			// 왼쪽 청소
 			cleanedArea++;
 			// 앞 쪽으로 한칸 이동 
 			yx = left;
 			// 해당 영역 청소 완료 표기
-			area[yx[0]][yx[1]]++;
+			area[yx[0]][yx[1]]--;
 		}
 		
 		return cleanedArea;
 	}
-	
-	// NESW (0123)
-	// d: N(0) -> c--
-	//    E(1) -> r++
-	//    S(2) -> c++
-	//    W(3) -> r--
+
 	private int[] cleanLeft(int[] yx, int d, int[][] area) {
 		int r = yx[0], c = yx[1];
 		int[] left = {r, c};
@@ -93,17 +95,21 @@ public class RobotVacuum {
 	private int[] shiftBack(int[] yx, int d, int[][] area) {
 		int r = yx[0], c = yx[1];
 		int[] shifted = {r, c};
-		
-		if (d == 0 && area[r + 1][c] == 0) {
+
+		if (d == 0 && r + 1 != area.length - 1) {
 			shifted[0]++;
-		} else if (d == 1 && area[r][c - 1] == 0) {
+		} else if (d == 1 && c - 1 != 0) {
 			shifted[1]--;
-		} else if (d == 2 && area[r - 1][c] == 0) {
+		} else if (d == 2 && r - 1 != 0) {
 			shifted[0]--;
-		} else if (d == 3 && area[r][c + 1] == 0) {
+		} else if (d == 3 && c + 1 != area[0].length - 1) {
 			shifted[1]++;
 		}
-		
+
+		if (shifted[0] == r && shifted[1] == c) {
+			shifted[0] = -2;
+			shifted[1] = -2;
+		}
 		return shifted;
 	}
 
