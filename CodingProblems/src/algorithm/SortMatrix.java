b8948ce002639bf994214b857282ad434689dd72
KsumiKim <ksumiKim@gmail.com>
2021-02-15 21:16:09 +0900

    Return given matrix sorted diagonally

--- CodingProblems/src/algorithm/SortMatrix.java
+++ CodingProblems/src/algorithm/SortMatrix.java
@@ -21,42 +21,62 @@ public class SortMatrix {
 				{84,28,14,11,5,50}
 		};
 		
-		diagonalSort(mat);
+		int[][] mat3 = {
+				{3,9},
+				{2,4},
+				{1,2},
+				{9,8},
+				{7,3}
+			};
 		
-		for (int[] nums : mat) {
+		diagonalSort(mat3);
+		
+		for (int[] nums : mat3) {
 			for (int n : nums)
 				System.out.print(n + ", ");
 			System.out.println();
 		}
 		
 	}
-	
+
     public static int[][] diagonalSort(int[][] mat) {
-
-    	for (int i = 0; i < mat[0].length - 1; i++) {
-    		sortDiagonally(mat, 0, i, true);
-    	}
-
-       	for (int i = 1; i < mat.length - 1; i++) {
-    		sortDiagonally(mat, i, 0, false);
-    	}
-
+    	
+    	for (int i = 0; i < mat[0].length - 1; i++)
+    		sortHorizontal(mat, i);
+    	
+    	for (int i = 1; i < mat.length - 1; i++)
+    		sortVertical(mat, i);
+    	
     	return mat;
     }
-	
-    private static void sortDiagonally(int[][] mat, int i, int j, boolean isHorizontal) {
-    	int limit = isHorizontal ? mat[0].length - 1 : mat.length - 1;
+    
+    private static void sortHorizontal(int[][] mat, int j) {
     	
-    	for (int y = 0; y < mat[0].length - 1; y++) {
+    	for (int a = 0; a < mat.length - 1; a++) {
     		
-    		for (int x = y + 1; j + x < limit; x++) {
-
-    			if (mat[i + y][j + y] > mat[i + x][j + x]) {
-    				int temp = mat[i + y][j + y];
-    				mat[i + y][j + y] = mat[i + x][j + x];
-    				mat[i + x][j + x] = temp;
+    		for (int b = a + 1; b < mat.length && b + j < mat[0].length; b++) {
+    			
+    			if (mat[a][a + j] > mat[b][b + j]) {
+    				int temp = mat[a][a + j];
+    				mat[a][a + j] = mat[b][b + j];
+    				mat[b][b + j] = temp;
     			}
     		}
     	}
     }
+    
+    private static void sortVertical(int[][] mat, int j) {
+    	
+    	for (int a = 0; a + j < mat.length - 1; a++) {
+    		
+    		for (int b = a + 1; b + j < mat.length && b < mat[0].length; b++) {
+    			
+    			if (mat[a + j][a] > mat[b + j][b]) {
+    				int temp = mat[a + j][a];
+    				mat[a + j][a] = mat[b + j][b];
+    				mat[b + j][b] = temp;
+    			}
+    		}
+    	}
+    } 
 }
