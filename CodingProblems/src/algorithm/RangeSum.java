83fbda83ac16f2eb20640c67cdee6e09628ec7c3
KsumiKim <ksumiKim@gmail.com>
2021-03-03 20:25:52 +0900

    Return sum of values between given ranges

--- CodingProblems/src/algorithm/RangeSum.java
+++ CodingProblems/src/algorithm/RangeSum.java
@@ -25,4 +25,23 @@ public class RangeSum {
 		
 		System.out.println(result);
 	}
+
+
+	static int sum = 0;
+	
+    public static int rangeSumBST(TreeNode root, int L, int R) {
+    	if (root == null)
+    		return 0;
+    	
+    	if (root.val >= L && root.val <= R)
+    		sum += root.val;
+    	
+    	if (root.val < R)
+    		rangeSumBST(root.right, L, R);
+    	
+    	if (root.val > L)
+    		rangeSumBST(root.left, L, R);
+    	
+    	return sum;
+    }
 }
