9901f408b2e7b31ec95e51cd374c43cb0b8680e2
KsumiKim <ksumiKim@gmail.com>
2021-03-15 20:26:42 +0900

    return average of each level of a given binary tree

--- CodingProblems/src/algorithm/AvgBinaryTree.java
+++ CodingProblems/src/algorithm/AvgBinaryTree.java
@@ -17,11 +17,13 @@ public class AvgBinaryTree {
 		root.right.left = new TreeNode(4);
 		root.right.right = new TreeNode(6);
 		
-		TreeNode root2 = new TreeNode(1);
-		root2.left = new TreeNode(1);
+		// [2147483647,2147483647,2147483647]
+		TreeNode root2 = new TreeNode(2147483647);
+		root2.left = new TreeNode(2147483647);
+		root2.right = new TreeNode(2147483647);
 		
 		AvgBinaryTree avgBinaryTree = new AvgBinaryTree();
-		List<Double> res = avgBinaryTree.avgOfTreeLevels(root);
+		List<Double> res = avgBinaryTree.averageOfLevels(root2);
 		
 		for (double d : res) {
 			System.out.print(d + ", ");
@@ -29,70 +31,31 @@ public class AvgBinaryTree {
 	}
 	
     public List<Double> averageOfLevels(TreeNode root) {
-     
-    	if (root == null)
-    		return null;
-    	
-    	Stack<Double> avgStack = new Stack<>();
-    	helper(root, avgStack);
-
-    	List<Double> result = new ArrayList<>();
-    	result.add((double) root.val);
-    	
-    	while (!avgStack.isEmpty()) {
-    		result.add(avgStack.pop());
-    	}
-    	
-    	return result;
-    }
-    
-    private int helper(TreeNode node, Stack<Double> avg) {
-    	
-    	if (node == null)
-    		return 0;
-    	
-    	int left = helper(node.left, avg);
-    	int right = helper(node.right, avg);
-    	
-    	int num = 0;
-    	
-    	num = left == 0 ? num : num + 1;
-    	num = right == 0 ? num : num + 1;
-    	    	
-    	if (left != 0 || right != 0) 
-    		avg.add((double)(left + right) / num);
-    	
-    	return node.val;
-    }
-    
-    public List<Double> avgOfTreeLevels(TreeNode root) {
-    	
-    	List<Integer> count = new ArrayList<>();
     	List<Double> res = new ArrayList<>();
+    	List<Double> countList = new ArrayList<>();
+    	List<Double> sumList = new ArrayList<>();
     	
-    	avg(root, 0, res, count);
+    	helper(root, countList, sumList, 0);
     	
-    	for (int i = 0; i < res.size(); i++) {
-    		res.set(i, res.get(i) / count.get(i));
-    	}
+    	for (int i = 0; i < countList.size(); i++) 
+    		res.add(sumList.get(i) / countList.get(i));
     	
     	return res;
     }
     
-    private void avg(TreeNode node, int i, List<Double> sum, List<Integer> count) {
-    	
+    private void helper(TreeNode node, List<Double> countList, List<Double> sumList, int depth) {
     	if (node == null)
     		return;
     	
-    	if (i < sum.size()) {
-    		sum.set(i, sum.get(i) + node.val);
-    		count.set(i, count.get(i) + 1);
+    	if (countList.size() <= depth) {
+    		countList.add(1.0);
+    		sumList.add(node.val * 1.0);
     	} else {
-    		sum.add(1.0 * node.val);
-    		count.add(1);
+    		countList.set(depth, countList.get(depth) + 1);
+    		sumList.set(depth, sumList.get(depth) + node.val);
     	}
     	
-    	avg(node.left, i + 1, sum, count);
-    	avg(node.right, i + 1, sum, count);
+    	helper(node.left, countList, sumList, depth + 1);
+    	helper(node.right, countList, sumList, depth + 1);
     }
 }
