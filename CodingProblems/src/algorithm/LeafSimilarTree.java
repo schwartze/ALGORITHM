21a25144f3a9261e50d187fe790eb071e337d293
KsumiKim <ksumiKim@gmail.com>
2021-03-16 21:28:12 +0900

    return if two given trees have the same sequence of leaves

--- CodingProblems/src/algorithm/LeafSimilarTree.java
+++ CodingProblems/src/algorithm/LeafSimilarTree.java
@@ -4,27 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LeafSimilarTree {
-    
-	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-		List<TreeNode> listForOne = new ArrayList<>();
-		List<TreeNode> listForTwo = new ArrayList<>();
-		
-		getLeaves(root1, listForOne);
-		getLeaves(root2, listForTwo);
-		
-		for (int i = 0; i < listForOne.size(); i++) 
-			if (listForOne.get(i) != listForTwo.get(i))
-				return false;
-
-		return true;
+	
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    	List<Integer> list1 = new ArrayList<>();
+    	List<Integer> list2 = new ArrayList<>();
+    	
+    	helper(root1, list1);
+    	helper(root2, list2);
+    	
+    	return list1.equals(list2);
     }
-
-	private void getLeaves(TreeNode node, List<TreeNode> leafList) {
-		
-		if (node.left == null && node.right == null)
-			leafList.add(node);
-		
-		getLeaves(node.left, leafList);
-		getLeaves(node.right, leafList);
-	}
+    
+    private void helper(TreeNode node, List<Integer> valueList) {
+    	if (node.left == null && node.right == null) {
+    		valueList.add(node.val);
+    		return;
+    	}
+    	if (node.left != null)
+    		helper(node.left, valueList);
+    	if (node.right != null)
+    		helper(node.right, valueList);
+    }
 }
