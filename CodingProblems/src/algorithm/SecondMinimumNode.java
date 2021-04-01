095121c623d01dd6286cec1a3366ea75e60bf264
KsumiKim <ksumiKim@gmail.com>
2021-04-01 21:45:55 +0900

    return second minimum node of a binary tree

--- CodingProblems/src/algorithm/SecondMinimumNode.java
+++ CodingProblems/src/algorithm/SecondMinimumNode.java
@@ -7,69 +7,43 @@ public class SecondMinimumNode {
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(2);
 		root.left = new TreeNode(2);
-		root.right = new TreeNode(5);
-		root.right.left = new TreeNode(5);
-		root.right.right = new TreeNode(7);
-		
-		TreeNode node = new TreeNode(2);
-		node.left = new TreeNode(2);
-		node.right = new TreeNode(5);
+		root.right = new TreeNode(2147483647);
+//		root.right.left = new TreeNode(5);
+//		root.right.right = new TreeNode(7);
+//		
+//		TreeNode node = new TreeNode(2);
+//		node.left = new TreeNode(2);
+//		node.right = new TreeNode(5);
 //		node.left.left = new TreeNode(5);
 //		node.right.right = new TreeNode(7);
 		
 		
 		SecondMinimumNode secondMin = new SecondMinimumNode();
-		int n = secondMin.findSecondMinimumValue(node);
+		int n = secondMin.findSecondMinimumValue(root);
 		System.out.println(n);
 	}
 	// [2,2,2147483647]
 	// [2,2,2]
 	// [2,2,5,null,null,5,7]
+    long min = Long.MAX_VALUE;
+    long secondMin = Long.MAX_VALUE;
+    
     public int findSecondMinimumValue(TreeNode root) {
-    	// nums[0] : min
-    	// nums[1] : secondMin
-    	int[] nums = {root.val, (int) Long.MAX_VALUE};
-    	
-    	helper(root, nums);
-    	
-    	return nums[1] < Long.MAX_VALUE ? nums[1] : -1;
+    	helper(root);
+    	return (int) (secondMin == Long.MAX_VALUE ? -1 : secondMin);
     }
     
-    private void helper(TreeNode node, int[] nums) {
-    	
+    private void helper(TreeNode node) {
     	if (node == null)
     		return;
     	
-    	helper(node.left, nums);
-    	if (node.val < nums[1] && node.val > nums[0]) {
-    		nums[1] = node.val;
+    	if (node.val < min) {
+    		secondMin = min;
+    		min = node.val;
+    	} else if (node.val > min && node.val < secondMin) {
+    		secondMin = node.val;
     	}
-    	helper(node.right, nums);
-    }
-    
-    
-    public int findSecondMinimumValue2(TreeNode root) {
-    	
-    	int max = root.val;
-    	int secondMax = -1;
-    	
-    	Stack<TreeNode> stack = new Stack<>();
-    	stack.push(root);
-    	
-    	while (!stack.isEmpty()) {
-    		TreeNode node = stack.pop();
-    		
-    		if (node.val > max) {
-    			secondMax = max;
-    			max = node.val;
-    		}
-    		
-    		if (node.left != null)
-    			stack.push(node.left);
-    		
-    		if (node.right != null)
-    			stack.push(node.right);
-    	}
-    	return secondMax;
+    	helper(node.left);
+    	helper(node.right);
     }
 }
