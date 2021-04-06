a1398a3185be3a7c37d7a57e97489548628211b3
KsumiKim <ksumiKim@gmail.com>
2021-04-06 23:51:00 +0900

    return words in given dictionary lists

--- CodingProblems/src/algorithm/WordsInDictionary.java
+++ CodingProblems/src/algorithm/WordsInDictionary.java
@@ -1,8 +1,10 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -12,83 +14,44 @@ public class WordsInDictionary {
 		String[] words = {"w","wo","wor","worl", "world"};
 		String[] words2 = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
 		String[] words3 = {"b","br","bre","brea","break","breakf","breakfa","breakfas","breakfast","l","lu","lun","lunc","lunch","d","di","din","dinn","dinne","dinner"};
+		String[] words4 = {"o","a","ajd","ajdpw","ojowj","okpnd","okpn","ef","oetj","ajdp","ojo","o","ok","oet","o","oj","ojowjy","e"};
+		String[] words5 = {"rac","rs","ra","on","r","otif","o","onpdu","rsf","rs","ot","oti","racy","onpd"};
+		String[] words6 = {"t","ti","tig","tige","tiger","e","en","eng","engl","engli","englis","english","h","hi","his","hist","histo","histor","history"};
 		WordsInDictionary dic = new WordsInDictionary();
-		String str = dic.longestWord2(words2);
+		String str = dic.findLongestWord(words4);
 		System.out.println(str);
+		
+		
 	}
-	
-    public String longestWord(String[] words) {
-        String ans = "";
-        Set<String> wordset = new HashSet<>();
-
-        for (String word: words) wordset.add(word);
-        
-        for (String word: words) {
-        
-        	if (word.length() > ans.length() ||
-                    word.length() == ans.length() && word.compareTo(ans) < 0) {
-                boolean good = true;
-            
-                for (int k = 1; k < word.length(); ++k) {
-                
-                	if (!wordset.contains(word.substring(0, k))) {
-                        good = false;
-                        break;
-                    }
-                }
-                if (good) ans = word;
-            }    
-        }
-        return ans;
-    }
-    
-    public String longestWord2(String[] words) {
-    	
-		char first = words[0].charAt(0);
-    	List<HashSet<String>> list = new ArrayList<>();
-    	
-    	int max = Integer.MIN_VALUE;
+	//"rac","rs","ra","r","onpdu","rsf","rs","oti","racy"
+	// "o", "on", "ot","oti","otif","onpd" 
+    public String findLongestWord(String[] words) {
+    	Map<Character, List<String>> map = new TreeMap<>();
+    	Arrays.sort(words);
     	String res = "";
-    	int len = 0;
     	
     	for (String word : words) {
-    		HashSet<String> set = new HashSet<>();
-
-    		if (word.charAt(0) != first) {
-    			first = word.charAt(0);
+    		if (!map.containsKey(word.charAt(0))) {
+        		List<String> wordList = new ArrayList<>();
+        		wordList.add(word);
+        		map.put(word.charAt(0), wordList);
+        		continue;
     		}
-			set.add(word);
-			String str = helper(set);
-
-			if (str.length() > max)
-				res = str;
-			max = Math.max(max, str.length());
+    		List<String> list = map.get(word.charAt(0));
+    		String lastWord = list.get(list.size() - 1);
+    		if (lastWord.length() == word.length() - 1 && 
+    				word.indexOf(lastWord) >= 0)
+    			list.add(word);
     	}
-      	
+    	int maxLength = 0;
+    	
+    	for (List<String> list : map.values()) {
+    		String lastWord = list.get(list.size() - 1);
+    		if (list.size() > maxLength && lastWord.length() == list.size()) {
+    			maxLength = list.size();
+    			res = lastWord;
+    		}
+    	}
     	return res;
-    }
-    
-    private String helper(HashSet<String> set) {
-    	
-    	String temp = "";
-    	
-    	for (String word : set) {
-    		
-    		if (!temp.equals(word.substring(0, temp.length())))
-    			continue;
-    		
-    		temp = word;
-    	}
-    	return temp;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }   
 }
