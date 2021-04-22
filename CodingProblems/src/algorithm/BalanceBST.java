fd463f1e5cf7972f1cd0e7ab16935cfb88872f10
KsumiKim <ksumiKim@gmail.com>
2021-04-23 00:59:44 +0900

    return a constructed balanced tree

--- CodingProblems/src/algorithm/BalanceBST.java
+++ CodingProblems/src/algorithm/BalanceBST.java
@@ -6,34 +6,55 @@ import java.util.List;
 
 public class BalanceBST {
 
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.right = new TreeNode(15);
+		root.right.left = new TreeNode(14);
+		root.right.right = new TreeNode(17);
+		
+		root.right.left.left = new TreeNode(7);
+		root.right.left.left.left = new TreeNode(2);
+		root.right.left.left.right = new TreeNode(12);
+		
+		root.right.left.left.left.right = new TreeNode(3);
+		root.right.left.left.right.left = new TreeNode(9);
+		root.right.left.left.right.left.right = new TreeNode(11);
+		
+		BalanceBST bBST = new BalanceBST();
+		bBST.balanceBST(root);
+	}
+	
     public TreeNode balanceBST(TreeNode root) {
         List<Integer> valueList = new ArrayList<>();
     	getAllValues(root, valueList);
     	Collections.sort(valueList);
     	
-    	int mid = valueList.size() / 2;
+    	int mid = valueList.size() / 2 - 1;
     	TreeNode newNode = new TreeNode(valueList.get(mid));
-    	buildBalancedBST(newNode, valueList, true, mid - 1);
-    	buildBalancedBST(newNode, valueList, false, mid + 1);
+    	buildBalancedBST(newNode, valueList, 0, mid);
+    	buildBalancedBST(newNode, valueList, mid, valueList.size());
     	
     	return newNode;
     }
-    private void buildBalancedBST(TreeNode node, List<Integer> list, boolean isLeft, int idx) {
-    	if (idx < 0 || idx >= list.size())
+    
+    private void buildBalancedBST(TreeNode node, List<Integer> list, int l, int r) {
+    	if (l >= r || node == null || r == 0)
+    		return;
+    	
+    	int mid = l == 0 ? ((r - l) / 2) : ((r - l) / 2 + l);
+    	if (list.get(mid) < 0)
     		return;
     	TreeNode temp = null;
-
-    	if (node.val > list.get(idx)) {
-    		temp = new TreeNode(list.get(idx));
+    	if (node.val > list.get(mid)) {
+    		temp = new TreeNode(list.get(mid));
     		node.left = temp;
-    		idx--;
-    		buildBalancedBST(temp, list, isLeft, idx);
     	} else {
-    		temp = new TreeNode(list.get(idx));
+    		temp = new TreeNode(list.get(mid));
     		node.right = temp;
-    		idx++;
-    		buildBalancedBST(temp, list, isLeft, idx);
     	}
+    	list.set(mid, -1);
+		buildBalancedBST(temp, list, l, mid);
+		buildBalancedBST(temp, list, mid, r);
     }
     
     private void getAllValues(TreeNode node, List<Integer> list) {
