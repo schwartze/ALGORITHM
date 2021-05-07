a2969908bc7f074acc88646d9fc51f5add0e2831
KsumiKim <ksumiKim@gmail.com>
2021-05-07 21:53:34 +0900

    return linked list where its ath to bth nodes replaced with list2

---
+++ CodingProblems/src/algorithm/MergeLinkedList.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+public class MergeLinkedList {
+
+	public static void main(String[] args) {
+		ListNode node1 = new ListNode(0);
+		node1.next = new ListNode(1);
+		node1.next.next = new ListNode(2);
+		node1.next.next.next = new ListNode(3);
+		node1.next.next.next.next = new ListNode(4);
+		node1.next.next.next.next.next = new ListNode(5);
+		int a = 3, b = 4;
+		
+		ListNode node2 = new ListNode(1000000);
+		node2.next = new ListNode(1000001);
+		node2.next.next = new ListNode(1000003);
+		
+		ListNode res = mergeInBetween(node1, a, b, node2);
+		
+		while (res != null) {
+			System.out.print(res.val + ", ");
+			res = res.next;
+		}
+	}
+	
+    public static ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+    	ListNode start = list1;
+    	ListNode end = list1;
+    	int idx = 0;
+    	
+    	while (idx++ < b) {
+    		if (idx < a) 
+    			start = start.next;
+    		end = end.next;
+    	}
+    	start.next = list2;
+    	ListNode temp = start.next;
+    	while (temp.next != null) {
+    		temp = temp.next;
+    	}
+    	temp.next = end.next;
+    	
+    	return list1;
+    }
+}
