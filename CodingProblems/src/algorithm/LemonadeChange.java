91d02a659f0993dfc452f8fdd27ee740be849394
KsumiKim <ksumiKim@gmail.com>
2021-07-06 21:23:13 +0900

    return true if every customer receives correct changes

--- CodingProblems/src/algorithm/LemonadeChange.java
+++ CodingProblems/src/algorithm/LemonadeChange.java
@@ -1,44 +1,54 @@
 package algorithm;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LemonadeChange {
 
 	public static void main(String[] args) {
 		int[] input = {5, 5, 10, 10, 20};
-		int[] bills = {5, 5, 10, 10, 20};
+		int[] input2 = {10, 10};
+		int[] input3 = {5, 5, 10};
+		int[] input4 = {5, 5, 5, 10, 20};
+		int[] input5 = {5,5,5,20,5,5,5,10,20,5,10,20,5,20,5,10,5,5,5,5};
+		int[] input6 = {5,5,5,5,5,5,5,5,5,5,5,5,5,5,20,5,5,5,5,5,5,5,10,5,20,20,5,5,5,5,5,10,5,5,5,20,5,5,5,10,5,5,10,5,20,5,5,20,5,10,5,5,20,5,5,5,5,5,5,10,20,5,20,20,10,5,20,20,5,10,5,5,5,5,5,5,20,20,20,20,5,5,10,5,20,5,5,5,5,10,10,5,5,5,20,5,5,5,5,5,5,20,5,20,10,10,20,5,5,5,5,20,20,5,5,5,5,20,5,20,20,5,5,10,5,5,5,20,5,5,20,5,5,5,5,5,5,5,5,5,5,5,5,20,5,5,10,20,20,5,5,10,20,5,5,5,5,10,20,5,5,10,20,5,10,10,5,5,5,5,5,5,10,10,10,5,10,5,10,5,5,10,10,5,5,5,20,5,20,10,20,5,5,5,20,10,5,5,20,5,5,5,10,5,5,10,5,5,20,5,10,10,5,5,10,5,5,10,5,10,5,20,10,5,10,10,5,5,5,5,10,5,5,5,20,5,5,5,5,10,5,10,10,5,20,20,5,10,10,10,5,10,5,10,5,10,5,5,10,5,5,5,20,5,5,20,5,5,5,5,5,5,10,5,5,20,20,5,5,5,5,10,5,5,5,20,5,5,5,5,10,20,5,5,5,20,20,5,10,5,5,5,10,5,10,20,20,5,5,5,5,5,5,20,10,5,10,5,5,20,10,5,5,5,20,5,5,5,5,5,5,20,5,5,5,5,5,5,5,5,5,5,5,5,5,5,10,10,5,10,5,10,20,10,10,5,5,20,10,20,5,5,5,10,5,5,5,10,5,20,20,20,10,20,5,5,5,5,20,5,20,5,10,5,5,5,5,5,5,20,5,10,5,5,5,20,5,5,5,10,10,5,5,5,5,5,20,20,20,5,5,5,5,20,5,20,5,20,20,10,10,5,5,5};		
 
 		LemonadeChange lChange = new LemonadeChange();
-		boolean res = lChange.lemonadeChange(input);
+		boolean res = lChange.lemonadeChange(input6);
 		System.out.println(res);
 	}
 	
 	// only takes $5, $10 or $20 bills 
-    public boolean lemonadeChange(int[] bills) {
-    	Map<Integer, Integer> billsMap = new HashMap<>();
-    	
-    	for (int bill : bills) {
-
-    		if (bill == 10) {
-    			int change = bill - 5;
-    			if (billsMap.get(change) == null || billsMap.get(change) <= 0)
-    				return false;
-    			billsMap.put(change, billsMap.get(change) - 1);
-    		}
-    		
-    		if (bill == 20) {
-    			if (billsMap.get(5) == null || billsMap.get(5) <= 0)
-    				return false;
-    			if (billsMap.get(10) == null || billsMap.get(10) <= 0)
-    				return false;
-    			billsMap.put(5, billsMap.get(5) - 1);
-    			billsMap.put(10, billsMap.get(10) - 1);
-    		}
-
-    		int count = billsMap.getOrDefault(bill, 0);
-    		billsMap.put(bill, count + 1);
-    	}
-    	return true;
+	public boolean lemonadeChange(int[] bills) {
+        int[] changes = new int[3];
+		boolean changeable = true;
+		
+		for (int bill : bills) {
+			
+			if (bill == 5) {
+				changes[0]++;
+			} else if (bill == 10) {
+				
+				if (changes[0] > 0) {
+					changes[0]--;
+					changes[1]++;
+				} else {
+					changeable = false;
+					break;
+				}
+			} else if (bill == 20) {
+				
+				if (changes[0] > 0 && changes[1] > 0) {
+					changes[0]--;
+					changes[1]--;
+					changes[2]++;
+				} else if (changes[0] >= 3) {
+					changes[0] -= 3;
+					changes[2]++;
+				} else {
+					changeable = false;
+					break;
+				}
+			}
+		}
+		
+    	return changeable;
     }
 }
