ed010dae8889e190ac813ee9fc0bf2d30108e46a
KsumiKim <ksumiKim@gmail.com>
2021-07-19 20:00:27 +0900

    return LCA of two given number in a tree

--- CodingProblems/src/algorithm/LowestCommonAncestor.java
+++ CodingProblems/src/algorithm/LowestCommonAncestor.java
@@ -1,10 +1,14 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LowestCommonAncestor {
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(6);
 		root.left = new TreeNode(2);
+		root.left.left = new TreeNode(0);
 		root.left.right = new TreeNode(4);
 		root.left.right.left = new TreeNode(3);
 		root.left.right.right = new TreeNode(5);
@@ -25,7 +29,7 @@ public class LowestCommonAncestor {
 		TreeNode q2 = new TreeNode(2);
 		
 		
-		TreeNode result = lowestCommonAncestor(root2, p2, q2);
+		TreeNode result = lowestCommonAncestorTree(root, p, q);
 		
 		System.out.println(result.val);
 	}
@@ -48,5 +52,39 @@ public class LowestCommonAncestor {
     		return root;
     	
     	return node;
+    }
+    
+    public static TreeNode lowestCommonAncestorTree(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> parentNodeListOfP = new ArrayList<>();
+        List<TreeNode> parentNodeListOfQ = new ArrayList<>();
+    	
+    	helper(root, p, parentNodeListOfP);
+    	helper(root, q, parentNodeListOfQ);
+    	
+    	TreeNode res = null;
+    	
+    	for (int i = 0; i < Math.min(parentNodeListOfP.size(), parentNodeListOfQ.size()); i++) {
+    		
+    		if (parentNodeListOfP.get(i) == parentNodeListOfQ.get(i))
+    			res = parentNodeListOfP.get(i);
+    		else
+    			break;
+    	}
+    	return res;
+    }
+    
+    private static void helper(TreeNode node, TreeNode target, List<TreeNode> list) {
+    	if (node == null)
+    		return;
+    	
+    	if (node.left == null && node.right == null)
+    		return;
+    	
+    	if (node.val == target.val)
+    		return;
+
+    	list.add(node);
+    	helper(node.left, target, list);
+    	helper(node.right, target, list);
     }
 }
