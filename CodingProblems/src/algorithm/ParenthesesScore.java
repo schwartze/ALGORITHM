52e23f2e16d04e1b6e12693d7d0313075d0b3ab4
KsumiKim <ksumiKim@gmail.com>
2021-08-27 20:58:57 +0900

    return score of parentheses

---
+++ CodingProblems/src/algorithm/ParenthesesScore.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Stack;
+
+public class ParenthesesScore {
+
+    public int scoreOfParentheses(String S) {
+        Stack<Integer> stack = new Stack<>();
+        int cur = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(') {
+                stack.push(cur);
+                cur = 0;
+            } else {
+                cur = stack.pop() + Math.max(cur * 2, 1);
+            }
+        }
+        return cur;
+    }
+}
