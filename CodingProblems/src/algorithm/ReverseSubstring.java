b2c46c69381a8b8170fcc02639fe427fc3cf38ce
KsumiKim <ksumiKim@gmail.com>
2021-08-30 21:10:57 +0900

    return reversed string in each pair of matching parenthess

---
+++ CodingProblems/src/algorithm/ReverseSubstring.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+import java.util.Stack;
+
+public class ReverseSubstring {
+
+    public static void main(String[] args) {
+		String s = "(ed(et(oc))el)";
+		
+		String res = reverseParentheses(s);
+		System.out.println(res);
+	}
+    
+    
+    private static String reverseParentheses(String s) {
+    	 int n = s.length();
+         Stack<Integer> opened = new Stack<>();
+         int[] pair = new int[n];
+     
+         for (int i = 0; i < n; ++i) {
+             if (s.charAt(i) == '(')
+                 opened.push(i);
+             if (s.charAt(i) == ')') {
+                 int j = opened.pop();
+                 pair[i] = j;
+                 pair[j] = i;
+             }
+         }
+         StringBuilder sb = new StringBuilder();
+         
+         for (int i = 0, d = 1; i < n; i += d) {
+             if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                 i = pair[i];
+                 d = -d;
+             } else {
+                 sb.append(s.charAt(i));
+             }
+         }
+         return sb.toString();
+    }
+}
