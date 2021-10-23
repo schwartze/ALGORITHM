8ee14b253ccbabb69ab7219d383662453d71f094
KsumiKim <ksumiKim@gmail.com>
2021-10-23 12:12:21 +0900

    return max XOR for each query (passed)

--- CodingProblems/src/algorithm/MaximumXOR.java
+++ CodingProblems/src/algorithm/MaximumXOR.java
@@ -6,26 +6,29 @@ public class MaximumXOR {
 		int[] nums = {0, 1, 1, 3};
 		int maximumBit = 2;
 		
-		int[] res = getMaximumXor(nums, maximumBit);
+		int[] nums2 = {2, 3, 4, 7};
+		int maximumBit2 = 3;
+		
+		int[] nums3 = {0, 1, 2, 2, 5, 7};
+		int maximumBit3 = 3;
+		
+		int[] res = getMaximumXor(nums3, maximumBit3);
 		for (int n : res)
 			System.out.print(n + ", ");
 	}
 
     public static int[] getMaximumXor(int[] nums, int maximumBit) {
     	int[] res = new int[nums.length];
-    	int k = 0;
+    	int max = (int)Math.pow(2, maximumBit) - 1;
+    	int prevAccumulated = 0;
     	
     	for (int i = 0; i < nums.length; i++) {
-    		int queryResult = 0;
-    		res[i] = k;
-    		
-    		for (int j = 0; j < nums.length - i; j++) {
-        		queryResult ^= nums[j];
-    		}
-    		queryResult ^= k;
-    		k = queryResult;
+    		int query = prevAccumulated ^ nums[i];
+    		res[nums.length - 1 - i] = query ^ max;
+    		prevAccumulated ^= nums[i];
     	}
     	
     	return res;
     }
+
 }
