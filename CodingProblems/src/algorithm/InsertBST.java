4187844644ee839fd96c37e0d23cf184e216627a
KsumiKim <ksumiKim@gmail.com>
2021-10-25 20:50:00 +0900

    return root node of BST after inserting additional node with given value

--- CodingProblems/src/algorithm/InsertBST.java
+++ CodingProblems/src/algorithm/InsertBST.java
@@ -1,40 +1,55 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class InsertBST {
 	
-	public TreeNode insertIntoBST(TreeNode root, int val) {
-        if (root == null)
-            return new TreeNode(val);
-        
-        boolean inserted = helper(root, val);
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(4);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(7);
+		
+		root.left.left = new TreeNode(1);
+		root.left.right = new TreeNode(3);
+		
+		InsertBST insertBST = new InsertBST();
+		TreeNode result = insertBST.insertIntoBST(root, 5);
+		System.out.println(result.val);
+	}
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+    	List<TreeNode> valList = new ArrayList<>();
+    	valList.add(new TreeNode(val));
+
+    	getTreeNodeValues(valList, root);
+    	Collections.sort(valList, (a, b) -> a.val - b.val);
+
+    	return buildTree(valList, 0, valList.size() - 1);
+    }
+    
+    private void getTreeNodeValues(List<TreeNode> list, TreeNode node) {
+    	if (node == null)
+    		return;
     	
-        if (!inserted) {
-    		insertAsLastNode(root.right, val);
-    	}
-        
+    	getTreeNodeValues(list, node.left);
+    	list.add(node);
+    	getTreeNodeValues(list, node.right);
+    }
+    
+    private TreeNode buildTree(List<TreeNode> list, int min, int max) {
+    	if (min > max)
+    		return null;
+
+    	int mid = (max - min) / 2 + min; 
+    	TreeNode root = list.get(mid);
+
+    	TreeNode left = buildTree(list, min, mid - 1); 
+    	TreeNode right = buildTree(list, mid + 1, max);
+    	root.left = left;
+    	root.right = right;
+    	
     	return root;
     }
-    
-    private void insertAsLastNode(TreeNode node, int val) {
-    	if (node == null) return;
-    	
-    	if (node.left == null && node.right == null) {
-    		node.right = new TreeNode(val);
-    		return;
-    	}
-    	insertAsLastNode(node.right, val);
-    }
-    
-    private boolean helper(TreeNode node, int val) {
-        if (node == null)
-    		return false;
-    	
-    	if (node.val > val && node.left == null) {
-    		node.left = new TreeNode(val);
-    		return true;
-    	}
-    		
-    	return helper(node.left, val) || helper(node.right, val);
-    }
-    
 }
