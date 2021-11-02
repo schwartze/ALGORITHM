657300f4461f7d77eac9e31de1b2b2936c0062fa
KsumiKim <ksumiKim@gmail.com>
2021-11-03 00:20:20 +0900

    search BST optimized

--- CodingProblems/src/algorithm/MinimumDifferenceBST.java
+++ CodingProblems/src/algorithm/MinimumDifferenceBST.java
@@ -10,41 +10,30 @@ public class MinimumDifferenceBST {
 		root.left.right = new TreeNode(3);
 		
 		MinimumDifferenceBST minDiff = new MinimumDifferenceBST();
-		minDiff.minDiffInBST2(root);
-		System.out.println(minDiff.min);
+		int res = minDiff.minDiffInBST(root);
+		System.out.println(res);
 	}
 	
-	static int diff = Integer.MAX_VALUE;
+	boolean init = false;
+	int min = Integer.MAX_VALUE;
+	int prev = 0;
 	
-    public int minDiffInBST(TreeNode root) {
-     
-    	if (root == null)
-    		return 0;
-    	
-    	int left = minDiffInBST(root.left);
-    	diff = Math.min(diff, Math.abs(left - root.val));
-    	int right = minDiffInBST(root.right);
-    	diff = Math.min(diff, Math.abs(right - root.val));
-    	
-    	return root.val;
-    }
-    
-    int min = Integer.MAX_VALUE,  prev = 0;
-    
-    public int minDiffInBST2(TreeNode root) {
-        
-    	if (root.left != null)
-    		minDiffInBST2(root.left);
-    	
-    	if (prev != 0)
-    		min = Math.min(min,  root.val - prev);
-    	 
-    	prev = root.val;
-    	
-    	if (root.right != null)
-    		minDiffInBST2(root.right);
-    	
-    	return min;
-    }
-    
+	public int minDiffInBST(TreeNode root) {
+		inorder(root);
+		return min;
+	}
+	
+	private void inorder(TreeNode node) {
+		if (node == null)
+			return;
+		
+		inorder(node.left);
+		if (!init) {
+			init = true; 
+		}
+		else {
+			min = Math.min(min, node.val - prev);
+		}
+		inorder(node.right);
+	}
 }
