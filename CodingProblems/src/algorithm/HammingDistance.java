7e97c89a6068f47fbaa48f701dfdf420ec3027f1
KsumiKim <ksumiKim@gmail.com>
2021-11-03 20:18:35 +0900

    return hamming distance between two given integers

--- CodingProblems/src/algorithm/HammingDistance.java
+++ CodingProblems/src/algorithm/HammingDistance.java
@@ -4,13 +4,12 @@ public class HammingDistance {
 
 	public static void main(String[] args) {
 		HammingDistance hd = new HammingDistance();
-		System.out.println(hd.getHammingDistance(1, 4));
+		System.out.println(hd.hammingDistance(3, 1));
 	}
 	
 	// Hamming distance between two integers is the number of 
 	// positions at which the corresponding bits are different
-    public int getHammingDistance(int x, int y) {
-       
+    public int hammingDistance(int x, int y) {
     	return Integer.bitCount(x ^ y);
     }
 }
