43c7b7aff73be6b46ba031dc53c983ff100e9aec
KsumiKim <ksumiKim@gmail.com>
2021-12-04 12:12:09 +0900

    return 32-bit integer after reversing it

--- CodingProblems/src/algorithm/ReverseInteger.java
+++ CodingProblems/src/algorithm/ReverseInteger.java
@@ -6,17 +6,21 @@ public class ReverseInteger {
 		int x = 123;
 		int x2 = -123;
 		int x3 = 120;
+		int x4 = -2147483648;
 		
-		int res = reverse(x3);
+		int res = reverse(x4);
 		System.out.println(res);
 	}
 	
 	public static int reverse(int x) {
-		StringBuilder sb = new StringBuilder();
-		int num = Math.abs(x);
+		long num = Math.abs((long)x);
+		if (Long.toString(num).length() < 2) {
+			return x;
+		}
 		
+		StringBuilder sb = new StringBuilder();
 		while (true) {
-			int mod = num % 10;
+			long mod = num % 10;
 			sb.append(mod);
 			num /= 10;
 			
@@ -25,7 +29,11 @@ public class ReverseInteger {
 				break;
 			}
 		}
-		return x > 0 ? Integer.parseInt(sb.toString()) : Integer.parseInt(sb.toString()) * -1;
+		
+		long reversedNum = x > 0 ? Long.parseLong(sb.toString()) : Long.parseLong(sb.toString()) * -1;
+		if (reversedNum > (long)Integer.MAX_VALUE || reversedNum < (long)Integer.MIN_VALUE)
+			return 0;
+
+		return (int)reversedNum;
     }
-    
 }
